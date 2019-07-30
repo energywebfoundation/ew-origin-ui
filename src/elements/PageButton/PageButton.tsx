5ff696e41890e8bd75eed10c74e5b6d4bbf50353
Joseph Bagaric <joseph@josephit.com>
2019-07-30 09:47:13 +0200

    Restore button

--- src/elements/PageButton/PageButton.tsx
+++ src/elements/PageButton/PageButton.tsx
@@ -103,6 +103,11 @@ export class PageButton extends React.Component<any, any> {
                     >
                         <DateRange onInit={this.handleDateRange} onChange={this.handleDateRange} />
                     </DropdownButton>
+                ) : type === 'button' ? (
+                    <button
+                        onClick={onClick}
+                        className={'btn btn-default'}
+                    >{label}</button>
                 ) : (
                     <div>Unknown Type</div>
                 )}
