04d874377f1843d57283c32b330ee03b2af3bcde
Daniel Kmak <neverkuzi@gmail.com>
2019-08-12 15:01:59 +0200

    Remember slider configuration

--- src/components/Table/IndividualFilter.tsx
+++ src/components/Table/IndividualFilter.tsx
@@ -1,73 +1,11 @@
 import React, { Component } from 'react';
 import { ICustomFilter, CustomFilterInputType } from './FiltersHeader';
-import { withStyles } from '@material-ui/core/styles';
-import Slider from '@material-ui/core/Slider';
-import { STYLE_CONFIG } from '../../styles/styleConfig';
+import { CustomSlider, CustomSliderThumbComponent } from '../CustomSlider';
 
 interface IProps {
     filter: ICustomFilter;
     changeFilterValue: (targetFilter: ICustomFilter, selectedValue: any) => void;
 }
-
-const CustomSlider = withStyles({
-    root: {
-      color: STYLE_CONFIG.PRIMARY_COLOR,
-      height: 3,
-      padding: '13px 0',
-    },
-    thumb: {
-      height: 27,
-      width: 27,
-      backgroundColor: '#2c2c2c',
-      border: '1px solid currentColor',
-      marginTop: -12,
-      marginLeft: -13,
-      '& .bar': {
-        height: 9,
-        width: 1,
-        backgroundColor: 'currentColor',
-        marginLeft: 1,
-        marginRight: 1,
-      },
-    },
-    active: {},
-    valueLabel: {
-        left: 'calc(-50% + 9px)',
-        top: -22,
-        '& *': {
-          background: 'transparent',
-          color: 'currentColor',
-        },
-      },
-    track: {
-      height: 3,
-    },
-    rail: {
-      color: '#393939',
-      opacity: 1,
-      height: 3,
-    },
-    mark: {
-        backgroundColor: 'currentColor',
-        height: 8,
-        width: 1,
-        marginTop: -3,
-    },
-    markActive: {
-        backgroundColor: 'currentColor',
-    },
-  })(Slider);
-
-function CustomSliderThumbComponent(props) {
-    return (
-      <span {...props}>
-          {props.children}
-        <span className="bar" />
-        <span className="bar" />
-        <span className="bar" />
-      </span>
-    );
-  }
 
 export class IndividualFilter extends Component<IProps> {
     toggleOption(targetFilter: ICustomFilter, value: any) {
@@ -86,7 +24,7 @@ export class IndividualFilter extends Component<IProps> {
 
         switch (filter.input.type) {
             case CustomFilterInputType.string:
-                return <input className="Filter_menu_item_input modal-input" onChange={(e) => this.props.changeFilterValue(filter, e.target.value)} />
+                return <input className="Filter_menu_item_input modal-input" onChange={(e) => this.props.changeFilterValue(filter, e.target.value)} value={filter.selectedValue ? filter.selectedValue : ''} />
             case CustomFilterInputType.multiselect:
                 return <div className="Filter_menu_item_options">
                     {filter.input.availableOptions.map((option, index) =>
@@ -103,7 +41,7 @@ export class IndividualFilter extends Component<IProps> {
                 return <div className="Filter_menu_item_sliderWrapper">
                     <CustomSlider
                         valueLabelDisplay="on"
-                        defaultValue={[filter.input.min, filter.input.max]}
+                        defaultValue={filter.selectedValue || [filter.input.min, filter.input.max]}
                         min={filter.input.min}
                         max={filter.input.max}
                         ThumbComponent={CustomSliderThumbComponent}
