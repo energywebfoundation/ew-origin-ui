41e98be1ebcf2db670eff7120a081f4fc7a151bd
Joseph Bagaric <joseph@josephit.com>
2019-08-14 11:02:34 +0200

    Change the way material theme is loaded to fix tests

--- src/styles/styleConfig.ts
+++ src/styles/styleConfig.ts
@@ -9,35 +9,38 @@ export const STYLE_CONFIG = {
     WHITE: '#fff'
 };
 
-export const DEFAULT_MATERIAL_THEME = createMuiTheme({
-    palette: {
-      primary: {
-        main: STYLE_CONFIG.PRIMARY_COLOR,
-        contrastText: STYLE_CONFIG.WHITE,
-      },
-      background: {
-        paper: "#2c2c2c",
-        default: "#f44336"
-      },
-      text: {
-          primary: STYLE_CONFIG.WHITE,
-          secondary: STYLE_CONFIG.GRAY_BORDER_COLOR,
-          hint: '#f50057',
-          disabled: '#f50057'
-      }
-    },
-    overrides: {
-        MuiInput: {
-            underline:{
-                '&:before': {
-                    borderBottom: '2px solid #474747'
-                }
-            },
+export const DEFAULT_MATERIAL_THEME = () => {
+    console.log(STYLE_CONFIG.PRIMARY_COLOR);
+    return createMuiTheme({
+        palette: {
+        primary: {
+            main: STYLE_CONFIG.PRIMARY_COLOR,
+            contrastText: STYLE_CONFIG.WHITE,
         },
-        MuiChip: {
-            root: {
-                marginRight: '10px'
+        background: {
+            paper: "#2c2c2c",
+            default: "#f44336"
+        },
+        text: {
+            primary: STYLE_CONFIG.WHITE,
+            secondary: STYLE_CONFIG.GRAY_BORDER_COLOR,
+            hint: '#f50057',
+            disabled: '#f50057'
+        }
+        },
+        overrides: {
+            MuiInput: {
+                underline:{
+                    '&:before': {
+                        borderBottom: '2px solid #474747'
+                    }
+                },
+            },
+            MuiChip: {
+                root: {
+                    marginRight: '10px'
+                }
             }
         }
-    }
-  });
+    });
+};
